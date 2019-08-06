65375f14c927e27901dd202812c1dbd07601e9be
ryancwilliams <rcw4rcw@gmail.com>
2019-08-05 23:59:02 -0400

    Create ItemRegistry.kt

---
+++ src/commonMain/kotlin/com/github/ryancwilliams/mcReliquaryPotionCalc/model/items/ItemRegistry.kt
@@ -0,0 +1,41 @@
+package com.github.ryancwilliams.mcReliquaryPotionCalc.model.items
+
+import com.github.ryancwilliams.mcReliquaryPotionCalc.model.registry.Registry
+
+/**
+ * A registry for all known items.
+ */
+object ItemRegistry : Registry<Item>() {
+
+    /**
+     * A index of all known items by name.
+     *
+     * This is used for quick access to the registry.
+     */
+    val indexByItemName = HashMap<String, Item>()
+
+    /**
+     * Gets a item by its name.
+     *
+     * @return The item or NULL if the item is not in this registry.
+     */
+    fun getItemByName(name: String) : Item? {
+        return this.indexByItemName.get(name)
+    }
+
+    override fun addToIndex(item: Item) {
+        this.addToIndexByItemName(item)
+    }
+
+    /**
+     * Adds this item to the by name index.
+     */
+    private fun addToIndexByItemName(item: Item) {
+        //Get the name
+        val name = item.itemName;
+
+        //Add to the index
+        this.indexByItemName.put(name, item)
+    }
+
+}
