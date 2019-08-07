a71d9701be21570b40050a5df372749bb05ce979
ryancwilliams <rcw4rcw@gmail.com>
2019-08-07 15:09:13 -0400

    Create PotionComponent.kt

---
+++ src/commonMain/kotlin/com/github/ryancwilliams/mcReliquaryPotionCalc/model/potions/PotionComponent.kt
@@ -0,0 +1,13 @@
+package com.github.ryancwilliams.mcReliquaryPotionCalc.model.potions
+
+/**
+ * A component used in potion essences.
+ */
+interface PotionComponent {
+
+    /**
+     * A set of all effects this component has
+     */
+    val effects: Set<PotionEffect>
+    
+}
